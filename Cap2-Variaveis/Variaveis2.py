a7fb2c26b32d36db35282083e2b5b42fe9b55bf3
Carla-Cordeiro <carlacordeiroads@gmail.com>
2023-01-07 08:20:04 -0300

    variaveis2

---
+++ Cap2-Variaveis/Variaveis2.py
@@ -0,0 +1,14 @@
+#Texto base
+#Declaro para o senhor Gonçalves Dias que o senhor Humberto Delgado esteve presente no evento SecurityCup e gastou o valor de R$ 30,00 com a entrada
+#Identificar: variáveis
+#Montar Projeto
+
+responsavel = input("Digite o nome do responsável: ")
+funcionario = input("Digite o nome do funcionário: ")
+evento = input("Digite o nome do evento: ")
+entrada = float(input("Digite o valor da entrada: "))
+
+print("O responsável " + responsavel + " foi informado que o funcionário " + funcionario + " esteve no evento " + evento + ".")
+print("No evento o funcionário pagou na entrada R$ ", entrada)
+
+
