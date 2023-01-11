afb04c4cdff4599669eaab7fc8ca398f41d4727d
Carla-Cordeiro <carlacordeiroads@gmail.com>
2023-01-11 08:48:58 -0300

    desafiowhile

---
+++ Cap2-Repeticoes/DesafioDecisaoWhile.py
@@ -0,0 +1,23 @@
+# Adicionar while a DesafioDecisoes
+
+resposta = "SIM"
+while resposta == "SIM":
+
+    nivel = input("Digite o nível de acesso (ADM, USR ou GUEST): ").upper()
+    if nivel == "ADM" or nivel == "USR":
+        genero = input("Digite gênero: ").upper()
+        if nivel == "ADM":
+            if genero == "FEMININO":
+                print("Olá administradora")
+            else:
+                print("Olá administrador")
+        else:
+            if genero == "FEMININO":
+                print("Olá usuária")
+            else:
+                print("Olá usuário")
+    elif nivel == "GUEST":
+        print("Olá visitante")
+    else:
+        print("Olá desconhecido (a)")
+    resposta = input("Digite SIM para continuar: ") .upper()
