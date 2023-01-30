17598b7cf6ddb8dde232b9567f05def8c7581f06
Carla-Cordeiro <carlacordeiroads@gmail.com>
2023-01-30 07:24:27 -0300

    situacoes

--- Cap3-Listas/MultiplasListas.py
+++ Cap3-Listas/MultiplasListas.py
@@ -23,3 +23,25 @@ for indice in range(0, len(equipamentos)):
     if busca == equipamentos[indice]:
         print("Valor: ", valores[indice])
         print("Serial: ", seriais[indice])
+
+depreciacao = input("\nDigite o nome do equipamento que será depreciado: ")
+for indice in range(0, len(equipamentos)):
+    if depreciacao == equipamentos[indice]:
+        valores[indice] = valores[indice] * 0.9
+        print("Novo valor: "), valores[indice]
+
+serial = int(input("\nDigite o serial do equipamento que será excluído: "))
+for indice in range(0, len(departamentos)):
+    if seriais[indice] == serial:
+        del departamentos[indice]
+        del equipamentos[indice]
+        del seriais[indice]
+        del valores[indice]
+        break
+
+for indice in range(0, len(equipamentos)):
+    print("\nEquipamento: ", (indice + 1))
+    print("Nome: ", equipamentos[indice])
+    print("Valor: ", valores[indice])
+    print("Serial: ", seriais[indice])
+    print("Departamento: ", departamentos[indice])
