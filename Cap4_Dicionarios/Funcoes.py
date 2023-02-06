d261c1571c6b9ec888e290e0a8b845b7e7e91f68
Carla-Cordeiro <carlacordeiroads@gmail.com>
2023-02-06 09:31:58 -0300

    funcoes

--- Cap4_Dicionarios/Funcoes.py
+++ Cap4_Dicionarios/Funcoes.py
@@ -1,2 +1,13 @@
-def perguntar ():
-    resposta = input("O que deseja realizar? \n"+)
+def perguntar():
+    resposta = input("O que deseja realizar? \n" +
+                     "<1> Para INSERIR um novo usuário\n" +
+                     "<2> Para PESQUISAR um usuário\n" +
+                     "<3> Para EXCLUIR um usuário\n" +
+                     "<4> Para LISTAR um usuário: ").upper()
+    return resposta
+
+
+def inserir(dicionario):
+    dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
+                                                     input("Digite a data de acesso: ").upper(),
+                                                     input("Digite a ultima estação acessada: ").upper()]
