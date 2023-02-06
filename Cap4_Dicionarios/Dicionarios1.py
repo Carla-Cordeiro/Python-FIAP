3bc059eda2ed7e7b5631fcda1bec6c3bb2d45122
Carla-Cordeiro <carlacordeiroads@gmail.com>
2023-02-06 08:15:29 -0300

    dicionarios

---
+++ Cap4_Dicionarios/Dicionarios1.py
@@ -0,0 +1,9 @@
+usuarios = {}
+usuarios = {
+    "Arya": ["Arya Stark", "15/06/289", "Recep_1"],
+    "Catelyn": ["Catelyn Stark", "13/02/264", "Raiox_02"]
+}
+
+print(usuarios)
+print("---------------------------")
+print("Dados: ", usuarios.get("Catelyn"))
